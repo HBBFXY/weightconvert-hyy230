59395699f3f4a2108fb80ce3d1247eac4a81809e
hyy230 <2638936581@qq.com>
2025-09-13 16:41:36 +0800

    Update main.py
    作业

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-Weight_str = input()
-    if Weight_str in ["kg"]:
-       pd = (eval(Weight_str[1]))*2.2046
-       print("pd")
-    elif Weight_str in ["pd"]:
-       pd = (eval(Weight_str[1]))/2.2046
-       print("kg")
+def main():
+    s=input().strip()
+    if s.endswith('kg'):
+        kg=float(s[:-2])
+        pd=kg*2.2046
+        print(f"对应的英制重量为{pd:.3f}镑")
+    elif s.endswith('pd'):
+        pd=float(s[:-2])
+        kg=pd/2.2046
+        print(f"对应的公制重量为{kg:.3f}公斤")
     else:
-       print("输入格式错误")
+        print("输入格式错误,请输入kg或pd结尾的字符串")
+if __name__=='__main__':
+    main()
