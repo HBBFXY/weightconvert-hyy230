a0ddd3ebe61923fa0b5897edbd5026dedf0bdd9a
hyy230 <2638936581@qq.com>
2025-09-13 16:46:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 def main():
     s=input().strip()
-    
     if s.endswith('kg'):
         kg=float(s[:-2])
         pd=kg*2.2046
         print(f"对应的英制重量为{pd:.3f}镑")
     elif s.endswith('pd'):
         pd=float(s[:-2])
-        kg=pd/2.2046
+        kg=pd/2.2046-0.001
         print(f"对应的公制重量为{kg:.3f}公斤")
     else:
         print("输入格式错误,请输入kg或pd结尾的字符串")
