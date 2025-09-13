d039d515ad75b04a93949a608b6fba70960b0ec1
hyy230 <2638936581@qq.com>
2025-09-13 16:51:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ def weight_converter():
     input_str=input().strip()
     
     if input_str.endswith('kg'):
-        kg=float(s[:-2])
+        kg=float(input_str[:-2])
         pd=kg*2.2046
         print(f"对应的英制重量为{pd:.3f}镑")
     elif input_str.endswith('pd'):
-        pd=float(s[:-2])
+        pd=float(input_str[:-2])
         kg=pd/2.2046-0.001
         print(f"对应的公制重量为{kg:.3f}公斤")
     else:
-        print("输入格式错误,请输入kg或pd结尾的字符串")
+        printf("输入格式错误,请输入kg或pd结尾的字符串")
 if __name__=="__main__":
     weight_converter()
